46472c455f12cfa8062a8146569f0a9c52982667
Akash Dwivedi <akashdwi17@gmail.com>
2025-10-27 10:42:19 +0530

    express with ejs

---
+++ ejsdir/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.listen(port, () => {
+  console.log(`Listining on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Root Directory");
+});
+
+app.get("/apple", (req, res) => {
+  res.render("apple.ejs");
+});
+
+app.get("/mango", (req, res) => {
+  res.send("Mango Page");
+});
+
+app.get("/papaya", (req, res) => {
+  res.send("Papaya Page");
+});
+
+app.get("/orange", (req, res) => {
+  res.send("Orange Page");
+});
+
+app.use((req, res) => {
+  res.send("Not a valid directory");
+});
